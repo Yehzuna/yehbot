ced745322e99afa498827e8772fe143ea0671776
Yehzuna <yehzuna@outlook.com>
2017-05-15 23:49:36 +0200

    Bits tests

--- src/js/YehBotCommands.js
+++ src/js/YehBotCommands.js
@@ -22,4 +22,43 @@ class YehBotCommands {
 
         this.overlay.appendChild(element);
     }
+
+    bits() {
+
+        let ul = document.querySelector(".bits");
+        if (ul === null) {
+            ul = document.createElement('ul');
+            ul.classList.add('bits');
+            this.overlay.appendChild(ul);
+        }
+
+        let data = [
+            {
+                name: "personne 1",
+                total: 200
+            }, {
+                name: "personne 2",
+                total: 100
+            }, {
+                name: "personne 3",
+                total: 100
+            }, {
+                name: "personne 4",
+                total: 100
+            },
+
+        ];
+
+        for (let i = 0; i < data.length; i++) {
+            let span = document.createElement('span');
+            span.innerHTML = data[i].name;
+
+            let li = document.createElement('li');
+            li.appendChild(span);
+
+            setTimeout(function () {
+                ul.appendChild(li);
+            }, 1000*i);
+        }
+    }
 }

0c098ad7af29db00198828d15b277d8d9e33dc66
Thomas Boruszewski <t.boruszewski@groupecerise.fr>
2017-05-17 15:59:09 +0200

    Add queue system

--- src/js/YehBotCommands.js
+++ src/js/YehBotCommands.js
@@ -1,6 +1,6 @@
 class YehBotCommands {
     constructor() {
-        this.overlay = document.querySelector('.overlay');
+        this.overlay = document.querySelector(".overlay");
 
         this.data = [
             {
@@ -18,6 +18,16 @@ class YehBotCommands {
             },
 
         ];
+
+        let ul = document.querySelector(".bits");
+        if (ul === null) {
+            ul = document.createElement('ul');
+            ul.classList.add('bits');
+            this.overlay.appendChild(ul);
+        }
+        this.parent = ul;
+
+        this.queue = [];
     }
 
     user(params) {
@@ -40,45 +50,98 @@ class YehBotCommands {
         this.overlay.appendChild(element);
     }
 
-    clean() {
-        const data = this.data;
-
-        let li = document.querySelectorAll(".bits li");
-        li.forEach(function (li , index) {
-            setTimeout(function () {
-               li.classList.add('clean');
-            }, 1000 * index);
+    addToQueue(fn, delay = 0) {
+        this.queue.push({
+            fn: fn,
+            delay: delay
         });
-
-
     }
 
+    playQueue() {
+        const _self = this;
+
+        console.log(_self.queue.length);
+        console.log("playQueue");
+
+        if (_self.queue.length > 0) {
+            let fn = _self.queue.shift();
+
+            setTimeout(function () {
+                fn['fn'].apply();
+
+                _self.playQueue();
+            }, fn['delay']);
+        } else {
+            _self.loop();
+        }
+    }
+
+    loop() {
+        const _self = this;
+
+        let loop = setInterval(function () {
+            console.log("loop");
+
+            if (_self.queue.length > 0) {
+                clearInterval(loop);
+                _self.playQueue();
+            }
+        }, 2000);
+    }
+
+    clean() {
+        const _self = this;
+
+        let els = document.querySelectorAll(".bits li");
+        els = Array.prototype.slice.apply(els).reverse();
+
+        els.forEach(function (li) {
+            _self.addToQueue(function () {
+                li.classList.add('clean');
+            }, 500);
+        });
+
+        els.forEach(function (li) {
+            _self.addToQueue(function () {
+                li.remove();
+            });
+        });
+    }
 
     bits() {
+        const _self = this;
+        const ul = this.parent;
+        const user = {
+            name: "Sollicitudin",
+            total: 100
+        };
 
-        let ul = document.querySelector(".bits");
-        if (ul === null) {
-            ul = document.createElement('ul');
-            ul.classList.add('bits');
-            this.overlay.appendChild(ul);
-        }
+        let span = `<span>${user.name} a donné ${user.total} bits !</span>`;
+        let li = document.createElement('li');
+        li.insertAdjacentHTML('afterbegin', span);
 
+        _self.clean();
+
+        _self.addToQueue(function () {
+            ul.insertBefore(li, ul.firstChild);
+        }, 1000);
+    }
+
+    list() {
+        const _self = this;
+        const ul = this.parent;
         const data = this.data;
 
+        _self.clean();
+
         data.forEach(function (user, index) {
-
-            let span = `<span>${index + 1} - ${user.name} (${user.total})</span>`;
-
+            let span = `<span>${data.length - index} - ${user.name} (${user.total})</span>`;
             let li = document.createElement('li');
             li.insertAdjacentHTML('afterbegin', span);
 
-            setTimeout(function () {
-                ul.prependChild(li);
-
-                setTimeout(function () {
-                    li.classList.add('opacity-' + index);
-                }, 1000);
-            }, 1000 * index);
+            _self.addToQueue(function () {
+                ul.insertBefore(li, ul.firstChild);
+            }, 1000);
         });
     }
 }

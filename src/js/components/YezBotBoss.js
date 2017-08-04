9d28f30813db0b5d33b6f023bc0c65c2bec2390d
Yehzuna <yehzuna@outlook.com>
2017-08-04 21:48:32 +0200

    Boss css demo

--- src/js/components/YezBotBoss.js
+++ src/js/components/YezBotBoss.js
@@ -1,30 +1,56 @@
-export class YezBotEmotes {
+export class YezBotBoss {
     constructor(channel) {
         this.channel = channel;
         this.overlay = document.querySelector(".overlay");
         this.boss = document.querySelector(".boss");
 
-
         const data = {
             name: "Un boss",
             hp: 900,
-            hp_max: 1000,
+            hpMax: 1000,
             mp: 20,
-            mp_max: 100,
+            mpMax: 100,
         };
 
         this.set(data);
     }
 
     set(data) {
+        if (data.hp > data.hpMax) {
+            data.hp = data.hpMax
+        }
+
+        if (data.hp < 0) {
+            data.hp = 0
+        }
+
+        if (data.mp > data.mpMax) {
+            data.mp = data.mpMax
+        }
+
+        if (data.mp < 0) {
+            data.mp = 0
+        }
+
         this.boss.querySelector(".title").innerHTML = data.name;
 
-        const hp = data.hp * 100 / data.hp_max;
+        // HP
+        const hp = data.hp * 100 / data.hpMax;
         this.boss.querySelector(".bar.hp .progress").style.width = `${hp}%`;
-        this.boss.querySelector(".status .hp").innerHTML= `HP: ${data.hp}/${data.hp_max}`;
+        this.boss.querySelector(".status .hp").innerHTML= `HP: ${data.hp}/${data.hpMax}`;
 
-        const mp = data.mp * 100 / data.mp_max;
+        this.boss.querySelector(".bar.hp").classList.remove('med');
+        this.boss.querySelector(".bar.hp").classList.remove('min');
+        if (hp <= 50) {
+            this.boss.querySelector(".bar.hp").classList.add('med');
+        }
+        if (hp <= 20) {
+            this.boss.querySelector(".bar.hp").classList.add('min');
+        }
+
+        // MP
+        const mp = data.mp * 100 / data.mpMax;
         this.boss.querySelector(".bar.mp .progress").style.width = `${mp}%`;
-        this.boss.querySelector(".status .mp").innerHTML= `MP: ${data.mp}/${data.mp_max}`;
+        this.boss.querySelector(".status .mp").innerHTML= `MP: ${data.mp}/${data.mpMax}`;
     }
 }
--- src/js/yezbot-boss.js
+++ src/js/yezbot-boss.js
@@ -1,4 +1,22 @@
+import {YezBotBoss} from "./components/YezBotBoss";
 
-document.querySelector("#refresh").addEventListener('click', function () {
+const boss = new YezBotBoss();
 
+document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let data = {};
+    let formData = new FormData(this);
+    for(let pair of formData.entries()) {
+        //console.log(pair);
+        if (pair[0] !== "name") {
+            pair[1] = parseInt(pair[1]);
+        }
+        data[pair[0]] = pair[1];
+    }
+
+    console.log(data);
+    boss.set(data);
+
+    return false;
 });
